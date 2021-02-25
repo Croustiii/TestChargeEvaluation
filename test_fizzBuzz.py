665243bffea91a00fb87ca975bd25bf09cd4b130
Sami DIEF <sami.dief@gmail.como>
2021-02-25 17:47:13 +0100

    Test Multipleof 5 : First Test Fail
    adding comments
    suprression of test with input 12 (same with test input 9)
    Adding First Test Multiple of 5

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -2,7 +2,7 @@ import unittest
 from fizzBuzz import *
 
 class FizzBuzzTest(unittest.TestCase):
-    
+    #Test Part Rule Multiple of 3
     def test_should_be_Fizz_with_input_6(self):
         expected = 'Fizz'
         actual = FizzBuzz.Respond(6)
@@ -19,7 +19,9 @@ class FizzBuzzTest(unittest.TestCase):
         actual = FizzBuzz.Respond(9)
         self.assertEqual(actual, expected)
     
-    def test_should_be_Fizz_with_multiple_of_3_with_input_12(self):
-        expected = 'Fizz'
-        actual = FizzBuzz.Respond(12)
+
+    #Test part Multiple of 5
+    def test_should_be_Buzz_with_input_5(self):
+        expected = 'Buzz'
+        actual = FizzBuzz.Respond(5)
         self.assertEqual(actual, expected)
