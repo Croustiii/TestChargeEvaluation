477983401a8fe73a22a38a7244db9c1ce38dc480
Sami DIEF <sami.dief@gmail.como>
2021-02-25 17:49:12 +0100

    Test Multiple of 5 : First Test Success

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -3,5 +3,7 @@ class FizzBuzz:
     def Respond(number):
         if (number % 3 == 0):
             return 'Fizz'
+        if (number  == 5):
+            return 'Buzz'
         else :
             return False
