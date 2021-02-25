05829e5efd64851bf381fddf72258e4e54592e6f
Sami DIEF <sami.dief@gmail.como>
2021-02-25 17:26:58 +0100

    Test Multiple of 3 : All test success but SMELL

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,7 +1,7 @@
 class FizzBuzz:
     @staticmethod
     def Respond(number):
-        if (number == 6):
+        if (number == 6 or number == 9):
             return 'Fizz'
         else :
             return False
