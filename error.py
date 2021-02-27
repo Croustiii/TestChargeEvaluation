2fe226d6b796e4e4666c56330ba5382558a8f454
Sami DIEF <sami.dief@gmail.como>
2021-02-27 11:27:08 +0100

    Code Coverage Report

--- error.py
+++ error.py
@@ -3,8 +3,13 @@ class ZeroErrorException(Exception):
     def __init__(self):
         self.__message = 'number is 0'
 
+    def message(self):
+        print(self.__message)  
 
 class NegativeErrorException(Exception):
 
     def __init__(self):
         self.__message = 'number is negative'
+    
+    def message(self):
+        print(self.__message)   
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+
+from fizzBuzz import *
+from error import *
+
 def main() :
-    pass
+    try :
+        number = input("Donnez moi un nombre !")
+        numInt = int(number)
+        print(FizzBuzz.Respond(numInt))
+    except Exception as e :
+        e.message()
 
 # Main
 if __name__ == "__main__":
-    main() 
+    main()
+
+
+
